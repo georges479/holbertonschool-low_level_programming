5210463f88863de46472f83dd709345094073d42
root <menheimgeorges83@gmail.com>
2024-10-22 10:13:29 +0000

    c4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+
+/**
+*main - is the entry point of a C program.
+*It's where the program starts executing.
+*When you run a C program, the operatind system calls the main function first.
+*Returne: Always 0.
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
