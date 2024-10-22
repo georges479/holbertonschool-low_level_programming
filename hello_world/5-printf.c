381f52e8097cf4f8c18c3896272cc6cc799c16c5
root <menheimgeorges83@gmail.com>
2024-10-22 11:06:23 +0000

    c5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+*main - Entry points of the program
+*
+*Returne: Always 0.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
