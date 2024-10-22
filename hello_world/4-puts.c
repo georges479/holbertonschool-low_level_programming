6c5629e5ffc474c6773bc4a95d120fae854e4347
root <menheimgeorges83@gmail.com>
2024-10-22 11:26:19 +0000

    c4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 
 /**
-*main - Entry points of the program
-*It's where the program starts executing.
-*When you run a C program, the operatind system calls the main function first.
-*Returne: Always 0.
-*/
+ * main - Entry point of the program
+ *
+ * This program prints a statement about art with proper grammar.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
