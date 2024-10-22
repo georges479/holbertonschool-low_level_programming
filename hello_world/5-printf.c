82ade85e79fe7404c504ee0b1a4106df259b1131
root <menheimgeorges83@gmail.com>
2024-10-22 11:20:30 +0000

    c5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 
 /**
-*Entry points of the program (main)
-*It ends the output with a newline (\n)
-*Returne: Always 0.
-*/
+ * main - Entry point of the program
+ *
+ * This program prints a statement about art with proper grammar.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
