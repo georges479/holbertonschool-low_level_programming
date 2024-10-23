9029170fc5e030d67064208a64892a0b7e5f8b32
root <menheimgeorges83@gmail.com>
2024-10-23 09:45:25 +0000

    c6_end

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
- 
+
 
 /**
- *C Program to Find the Size of int, float, double, and char using sizeof operator directly
+ * main - Entry point of the program
+ *
+ * C Program to Find the Size of int,
+ *
+ * float, double, and char using sizeof operator directly
+ *
+ * Return: Always returns 0 to indicate success.
  */
 int main(void)
 {
-printf("Size of int: %u bytes\n", sizeof(int));
-printf("Size of float: %u bytes\n", sizeof(float));
-printf("Size of char: %u bytes\n", sizeof(char));
-printf("Size of long int: %u bytes\n", sizeof(long int));
-printf("Size of long int: %u bytes\n", sizeof(long long int));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long int: %zu bytes\n", sizeof(long long int));
 return (0);
 }
 
