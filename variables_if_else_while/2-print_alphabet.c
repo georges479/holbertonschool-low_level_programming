70e258a8056c98dc6ffe4fd7c4751e86993ab6b4
root <menheimgeorges83@gmail.com>
2024-10-24 10:25:01 +0000

    2c2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(void)
+{
+	char c;
+	for(c = 'a'; c<='z';c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+	
