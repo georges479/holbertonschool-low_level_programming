b3752a51c868922694f9713663598de2faa309d5
root <menheimgeorges83@gmail.com>
2024-10-24 13:54:48 +0000

    2c5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,13 @@
  * Return: Always returns 0 to indicate success.
  */
 
-int main (void)
+int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
 	printf("\n");
 	return (0);
