916fd8cc7a79e9072e9d5e5ce6a5c29e965880e5
root <menheimgeorges83@gmail.com>
2024-10-24 20:08:29 +0000

    2c_end

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(void)
+
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
