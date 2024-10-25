70adef893d0ee1707486ad17e5dbc2a76bf69591
root <menheimgeorges83@gmail.com>
2024-10-25 10:51:23 +0000

    2c_end

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,16 +10,13 @@ int main(void)
 
 {
 	int i;
-	int j;
 
-	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (i = 0; i < 10; i++)
 		{
 			putchar(i + '0');
-			putchar(j + '0');
 
-			if (i != 8 || j != 9)
+			if (i != 9)
 			{
 				putchar(',');
 				putchar(' ');
