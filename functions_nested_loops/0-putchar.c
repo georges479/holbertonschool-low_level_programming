617805749d58daabc853b7455015dd7300c3dbb6
root <menheimgeorges83@gmail.com>
2024-10-25 12:05:52 +0000

    3c0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ *
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
