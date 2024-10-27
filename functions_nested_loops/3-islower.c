cc0227c31d5dedccee666e3c4b6b1a4c7ce1b2c9
root <menheimgeorges83@gmail.com>
2024-10-27 12:50:57 +0000

    3c3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * _islower - checks if characters are lowercase
  *
@@ -8,29 +6,12 @@
  *
  * @c parameter: checking c characters
  *
- * Return: Always 0.
+ * Return: 1 if char in lowercase, otherwise 0 if uppercase
  */
 int _islower(int c)
 {
-
-	c = _islower('I');
-	_putchar(c + '0');
-	c = _islower('a');
-	_putchar(c + '0');
-	c = _islower('s');
-	_putchar(c + '0');
-	c = _islower(70);
-	_putchar(c + '0');
-	c = _islower(20);
-	_putchar(c + '0');
-	c = _islower('U');
-	_putchar(c + '0');
-	c = _islower('n');
-	_putchar(c + '0');
-	c = _islower(0);
-	_putchar(c + '0');
-	c = _islower('H');
-	_putchar(c + '0');
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
