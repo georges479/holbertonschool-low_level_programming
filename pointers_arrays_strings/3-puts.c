462c370e0e40427df223d461c63e12fb860eb84a
root <menheimgeorges83@gmail.com>
2024-11-01 19:05:58 +0000

    3p3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 
+/**
+ * _puts - Affiche une chaîne de caractères suivie d'un saut de ligne
+ * @str: Pointeur vers la chaîne à affich
+ */
 void _puts(char *str)
 {
-	char *str = "stdout"
-	puts(stdout)
-	printf("\n")
+	if (str == NULL)
+	return;
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
