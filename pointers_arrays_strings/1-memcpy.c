ac24217fa52cde0a0970f83ed7244d24f341c53b
root <menheimgeorges83@gmail.com>
2024-11-04 11:34:20 +0000

    5p1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,6 +2,13 @@
 
 
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
