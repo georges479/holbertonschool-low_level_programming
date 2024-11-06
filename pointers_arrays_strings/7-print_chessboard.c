cb5f71bcbe4e859594cbbe35875f3b2e232660bd
root <menheimgeorges83@gmail.com>
2024-11-06 10:55:52 +0000

    5p7(6)

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * print_chessboard - Affiche un échiquie
+ * @a: Tableau 2D représentant l'échiqui
+ *
+ * Cette fonction parcourt chaque élément du table
+ * et l'affiche sous forme d'échiquie
+ */
 void print_chessboard(char (*a)[8])
 {
-	printf("%c", a[8]);
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
 }
