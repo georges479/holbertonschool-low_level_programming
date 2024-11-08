ef76e5a45576311460a00cd0bbaf7e182d6411df
root <menheimgeorges83@gmail.com>
2024-11-08 15:56:09 +0000

    a_a2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints all arguments it receives
+ * @argc: The number of command line arguments
+ * @argv: The array of command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}	
+	return (0);
+}
