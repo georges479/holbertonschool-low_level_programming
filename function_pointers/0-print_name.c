157ff76def1db14a1f9214077dd6e19dc66d7370
root <menheimgeorges83@gmail.com>
2024-11-18 11:42:38 +0000

    fp0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+
+/**
+ * print_name - Prints a name using a given function pointer.
+ * @name: The name to be printed.
+ * @f: A pointer to the function used for printing.
+ *
+ * Description: This function checks if the pointers are valid and
+ *              calls the function `f` to print the name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
---
+++ function_pointers/README.h
@@ -0,0 +1,1 @@
+function to a pointer
