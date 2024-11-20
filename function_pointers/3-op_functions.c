fbc86d37418350b088dc6cb0ac370bc9a160cdf5
root <menheimgeorges83@gmail.com>
2024-11-20 12:48:13 +0000

    stape 1&2 of task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,80 @@
+#include "calc.h"
+#include <stdio.h>
+
+
+/**
+ * op_add - Returns the sum of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - Returns the difference of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - Returns the product of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - Returns the division of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The result of the division.
+ *         Prints an error and exits with status 100 if b is 0.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	return (a / b);
+}
+
+
+/**
+ * op_mod - Returns the remainder of the division of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The remainder of the division.
+ *         Prints an error and exits with status 100 if b is 0.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	return (a % b);
+}
