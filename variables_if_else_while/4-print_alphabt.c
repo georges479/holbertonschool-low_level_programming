5976434ae71cd28dbd8cb6692fa91204d1fb1cc9
georges479 <g.ismael071221@gmail.com>
2025-02-20 11:52:05 +0100

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+	{
+		if (alpha != 101 && alpha != 113)
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
