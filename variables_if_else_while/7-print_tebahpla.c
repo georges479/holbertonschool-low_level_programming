c812b0a79b7650d2ee9b8b73c9c0aa750cdcf48b
georges479 <g.ismael071221@gmail.com>
2025-02-21 08:49:46 +0100

    7. Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet reverse in lowercase
+ * 
+ * Return: always 0
+ */
+int main(void)
+{
+    char alpha = 122
+
+    while (alpha => 61)
+    {
+        alpha--;
+        putchar(alpha);
+    }
+    putchar('\n');
+    return (0);
+}
