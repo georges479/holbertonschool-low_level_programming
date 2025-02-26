6c99259d15e0f8198dda577db258999b25d7ae65
georges479 <g.ismael071221@gmail.com>
2025-02-26 12:55:25 +0100

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i > 100)
+		{
+			printf("\n");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
