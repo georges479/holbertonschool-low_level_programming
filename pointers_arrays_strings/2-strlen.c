37b74659cb83e7c1dacc9d842c78ef32a557d877
georges479 <g.ismael071221@gmail.com>
2025-02-27 15:50:06 +0100

    2. This report, by its very length, defends itself against the risk of being read

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,14 +5,17 @@
  * _strlen - Returns the length of a string.
  * @s: Pointer to the string.
  *
- * Description: This function calculates the length of the given
- * string and returns it as an integer.
+ * Description: This function counts the number of characters in
+ * the given string until it reaches the null terminator '\0'.
  * Return: The length of the string.
  */
 int _strlen(char *s)
 {
 	int len;
 
-	len = strlen(s);
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
 	return (len);
 }
