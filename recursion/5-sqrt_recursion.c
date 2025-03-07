7275686915c934b9130e6416d957396928301e66
georges479 <g.ismael071221@gmail.com>
2025-03-07 13:10:04 +0100

    5. Your subconscious is looking for the dreamer

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,23 +3,23 @@
 /**
  * helper - Aide récursive pour trouver la racine carrée naturelle
  * @n: Le nombre dont on cherche la racine carrée
- * @guess: Le nombre à tester comme racine
+ * @num: Le nombre à tester comme racine
  *
  * Return: La racine carrée si elle existe, sinon -1
  */
-int helper(int n, int guess)
+int helper(int n, int num)
 {
-	if (guess * guess > n)
+	if (num * num > n)
 	{
 		return (-1);
 	}
-	else if (guess * guess == n)
+	else if (num * num == n)
 	{
-		return (guess);
+		return (num);
 	}
 	else
 	{
-		return (helper(n, guess + 1));
+		return (helper(n, num + 1));
 	}
 }
 
