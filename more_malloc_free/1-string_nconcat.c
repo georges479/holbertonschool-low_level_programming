10ab143e1bd7d71cb01d1976a3d222a73d662ed9
georges479 <g.ismael071221@gmail.com>
2025-03-13 13:48:14 +0100

    1. string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2;
+	unsigned int i, j, len1 = 0, len2 = 0;
 	char *ptr;
 
 	if (s1 == NULL)
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len2++;
 	}
-	if (n > len2)
+	if (n >= len2)
 	{
 		n = len2;
 	}
