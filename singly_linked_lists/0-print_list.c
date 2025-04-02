21a6c36823cb6ddc0de2ed04d0958c5d44ec68dd
georges479 <g.ismael071221@gmail.com>
2025-04-02 22:12:13 +0200

    fixed task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,16 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		nodes++;
 		h = h->next;
