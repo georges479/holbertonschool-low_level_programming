1922e33dffba053a44cebe5a5048a1f3554582ac
georges479 <g.ismael071221@gmail.com>
2025-04-02 14:12:44 +0200

    fixed task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	char *new_str;
 	list_t *new_node;
+	unsigned int len = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -23,11 +24,13 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(new_str);
+	while (new_str[len])
+		len++;
+
 	new_node->str = new_str;
-
+	new_node->len = len;
 	new_node->next = *head;
-	*head = new_node;
 
+	*head = new_node;
 	return (new_node);
 }
